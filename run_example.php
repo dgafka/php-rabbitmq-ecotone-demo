fd8a06bc23a1df44200b8050d9bf9170671aff56
Dariusz Gafka <dariuszgafka@gmail.com>
2022-02-18 18:52:08 +0100

    readme

--- run_example.php
+++ run_example.php
@@ -7,9 +7,12 @@ use Enqueue\AmqpExt\AmqpConnectionFactory;
 require __DIR__ . "/vendor/autoload.php";
 
 $application = EcotoneLiteApplication::boostrap([AmqpConnectionFactory::class => new AmqpConnectionFactory("amqp://guest:guest@rabbitmq:5672//")]);
+
+// Send
 $messagePublisher = $application->getMessagePublisher();
 
 $messagePublisher->convertAndSend(["personId" => 1, "product" => "milk"]);
 $messagePublisher->convertAndSend(["personId" => 2, "product" => "bread"]);
 
+// Receive
 $application->run(Configuration::CONSUMER_NAME);
